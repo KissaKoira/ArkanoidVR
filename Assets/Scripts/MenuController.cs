5cc140484ef2c57a4394ee9789dfb495f32562ac
hataramiko <56047893+hataramiko@users.noreply.github.com>
2020-02-05 22:04:41 +0200

    Started menu stuff

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+
+public class MenuController : MonoBehaviour
+{
+    // Does GameObject stuff
+    public GameObject pauseMenuUI;
+    public GameObject optionsUI;
+    public GameObject loseUI;
+    public GameObject winUI;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 1f;
+
+        pauseMenuUI.SetActive(false);
+        optionsUI.SetActive(false);
+        loseUI.SetActive(false);
+        winUI.SetActive(false);
+    }
+
+
+    // Method called when game is paused
+    void Paused()
+    {
+        Time.timeScale = 0f;
+
+        pauseMenuUI.SetActive(true);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
