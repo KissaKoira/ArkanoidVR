3551ea459dafe3dd99eb17d7a68a5547fd0b87a0
Waltteri777 <47692321+Waltteri777@users.noreply.github.com>
2020-02-12 10:40:36 +0200

    Dublicate powerup under construction

---
+++ Assets/Scripts/PowerUpDublicate.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUpDublicate : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.name == "Ball")
+        {
+            Dublicate();
+
+        }
+
+    }
+    Dublicate(Collider Ball)
+    {
+
+        BallInstance = 
+
+        yield return new WaitForSeconds(duration);
+
+        Ball.transform.localScale /= multiplier;
+    }
+
+}
