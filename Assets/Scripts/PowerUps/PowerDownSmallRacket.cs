8ae45fc6562ed401b3a1f7fc91d934a51dd1919d
Waltteri777 <jultteriwannila@gmail.com>
2020-03-04 16:50:02 +0200

    powerdownsmallball

---
+++ Assets/Scripts/PowerUps/PowerDownSmallRacket.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerDownSmallRacket : MonoBehaviour
+{
+    public float multiplier = 1.5f;
+    public float duration = 2.0f;
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.name == "Ball")
+        {
+            GetComponent<Animator>().SetTrigger("Hit");
+
+            StartCoroutine(SmallRacket(other));
+
+        }
+
+    }
+    //makes ball bigger
+    IEnumerator SmallRacket(Collider Ball)
+    {
+
+        Racket.transform.localScale *= multiplier;
+
+        yield return new WaitForSeconds(duration);
+
+        Racket.transform.localScale /= multiplier;
+    }
+}
