61f08db7fda540f15e3d9a157fa259524d4c878e
hataramiko <56047893+hataramiko@users.noreply.github.com>
2020-03-06 00:45:50 +0200

    Menu and localization shit

---
+++ Assets/Scripts/LanguageData.cs
@@ -0,0 +1,13 @@
+﻿[System.Serializable]
+public class LanguageData
+{
+    public LanguageItem[] items;
+}
+
+[System.Serializable]
+public class LanguageItem
+{
+    public string key;
+    public string value;
+}
+
---
+++ Assets/Scripts/StartUpManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartUpManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private IEnumerator Start()
+    {
+        while(!LanguageManager.instance.GetIsReady())
+        {
+            yield return null;
+        }
+        SceneManager.LoadScene ("Main Menu");
+    }
+}
