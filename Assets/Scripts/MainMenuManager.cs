cb15ba27dd1c4d655d25a520f11ff6643dbf4f7c
hataramiko <56047893+hataramiko@users.noreply.github.com>
2020-03-09 14:16:01 +0200

    stuff

---
+++ Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+
+public class MainMenuManager : MonoBehaviour
+{
+    public GameObject mainMenuUI;
+    public GameObject levelSelectUI;
+    public GameObject levelInfoUI;
+    public GameObject optionsUI;
+    public GameObject creditsUI;
+    public GameObject exitUI;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 1f;
+
+        //mainMenuUI.SetActive(false);
+        levelSelectUI.SetActive(false);
+        levelInfoUI.SetActive(false);
+        optionsUI.SetActive(false);
+        creditsUI.SetActive(false);
+        exitUI.SetActive(false);
+    }
+
+    // Method called when entering main menu
+    public void MainMenu()
+    {
+        mainMenuUI.SetActive(true);
+
+        optionsUI.SetActive(false);
+        creditsUI.SetActive(false);
+        levelSelectUI.SetActive(false);
+        exitUI.SetActive(false);
+    }
+
+
+    // Method called when entering level select
+    public void LevelSelect()
+    {
+        levelSelectUI.SetActive(true);
+        mainMenuUI.SetActive(false);
+    }
+
+
+    // Method called when entering options
+    public void OptionsOpen()
+    {
+        optionsUI.SetActive(true);
+        mainMenuUI.SetActive(false);
+    }
+
+    // Method called when exiting options
+    public void OptionsClose()
+    {
+        optionsUI.SetActive(false);
+    }
+
+
+    // Method called when opening credits
+    public void Credits()
+    {
+        creditsUI.SetActive(true);
+        mainMenuUI.SetActive(false);
+    }
+
+
+    // Method called when selecting exit
+    public void Exit()
+    {
+        exitUI.SetActive(true);
+    }
+
+    // Method called when closing the game
+    public void TerminateGame()
+    {
+        // Enter game closing code here //
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
