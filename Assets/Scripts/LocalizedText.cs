2844cb7ac604d9fea5290882367a2deb34cec33d
hataramiko <56047893+hataramiko@users.noreply.github.com>
2020-03-10 22:11:06 +0200

    Added localization

---
+++ Assets/Scripts/LocalizedText.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class LocalizedText : MonoBehaviour
+{
+    public string key;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        TextMeshProUGUI text = GetComponent<TextMeshProUGUI>();
+        Debug.Log(text);
+        text.text = LocalizationManager.instance.GetLocalizedValue(key);
+    }
+}
