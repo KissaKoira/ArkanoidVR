a1906f319c00fbfb363d5654c5689983c78b1f2c
Waltteri777 <jultteriwannila@gmail.com>
2020-03-20 08:33:16 +0200

    healthsystembrokenfirstcommit

---
+++ Assets/Health.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+
+public int maxHealth = 100;
+public int currentHealth;
+public HealthBar healthBar;
+    void Start()
+    {
+        currentHealth = maxHealth;
+        HealthBar.SetMaxHealth(maxHealth);
+    }
+
+    // Update is called once per frame
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "ball")
+
+        {
+            TakeDamage(25);
+        }
+    }
+
+    void TakeDamage(int damage)   
+{
+    currentHealth -= damage;
+
+    HealthBar.SetHealth(currentHealth);
+}
+}
